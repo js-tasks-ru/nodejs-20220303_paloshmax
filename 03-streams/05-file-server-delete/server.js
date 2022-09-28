2426955e560c2052a756164a85b5ec514283be06
dSalieri <paloshmax@gmail.com>
2022-09-29 09:56:24 +1000

    Streams have been solved

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +12,26 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+      if (pathname.match(/\//g)?.length > 0) {
+        res.statusCode = 400;
+        res.end('Bad request');
+        return;
+      }
+      fs.access(filepath, (err) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File is not exist');
+            return;
+          }
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+          return;
+        }
+        fs.unlink(filepath, () => {});
+        res.statusCode = 200;
+        res.end('File deleted');
+      });
       break;
 
     default:
